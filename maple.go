94e6ea49e09c5714f25ec7caff9af8d7de33a45d
honmaple <xiyang0807@gmail.com>
2018-09-12 14:06:47 +0800

    update

---
+++ maple.go
@@ -0,0 +1,18 @@
+/*********************************************************************************
+ Copyright © 2018 jianglin
+ File Name: maple.go
+ Author: jianglin
+ Email: mail@honmaple.com
+ Created: 2018-09-12 14:03:54 (CST)
+ Last Update: Wednesday 2018-09-12 14:06:39 (CST)
+		  By:
+ Description:
+ *********************************************************************************/
+package maple
+
+import "fmt"
+
+// Hello ..
+func Hello() {
+	fmt.Println("Hello ...")
+}
